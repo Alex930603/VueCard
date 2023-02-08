a9a2dae043fa2d135cb8ee5cdb81fd87d04f3ac8
Alessandrob <alessandro.bacci1993@gmail.com>
2023-02-08 10:57:38 +0100

    Some comments

--- src/components/partials/Bottone.vue
+++ src/components/partials/Bottone.vue
@@ -1,10 +1,13 @@
 <template>
 
   <button :class="{
-    [bgColor]: true,
+    [bgColor]: true,        /* Quando viene dato un valore alla props bgColor, quello verrà inserito nella classe tailwind del bottone */
     [fontColor]: true,
     'text-white': !fontColor,
-  }" class="font-bold py-2 px-10 rounded">{{ functionality }}</button>
+  }" class="font-bold py-2 px-10 rounded">{{ functionality }}
+    <!-- props functionality value can be declared in other page,like this we can create differnt buttons easy -->
+
+  </button>
 
 </template>
 
